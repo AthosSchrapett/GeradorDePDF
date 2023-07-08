2c7ba3c39360bd076f8887b2181d9afc9e43ca7a
Athos Schrapett <athosschrapett@outlook.com>
2023-07-08 08:49:20 -0300

    initial create

---
+++ GeradorDePDF.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace GeradorDePDF.API.Extensions;
+
+public static class SwaggerExtension
+{
+    public static void AddSwaggerDoc(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Gerador de PDF",
+                Description = "API para gerar PDF.",
+                Version = "v1",
+                Contact = new OpenApiContact
+                {
+                    Name = "Gerador de PDF",
+                }
+            });
+        });
+    }
+
+    public static void UseSwaggerDoc(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "GeradorDePDF.API");
+        });
+    }
+}
---
+++ GeradorDePDF.API/Models/ModelTxt.cs
@@ -0,0 +1,13 @@
+﻿namespace GeradorDePDF.API.Models;
+
+public class ModelTxt
+{
+    public string? Title { get; set; }
+    public IEnumerable<string>? Conteudo { get; set; }
+
+    public ModelTxt(string? title, IEnumerable<string>? conteudo)
+    {
+        Title = title;
+        Conteudo = conteudo;
+    }
+}
