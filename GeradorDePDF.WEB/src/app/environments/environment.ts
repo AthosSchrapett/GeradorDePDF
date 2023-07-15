8bfe4cf30e4f09a86425ba16ac1542f611129ffe
Athos Schrapett <athosschrapett@outlook.com>
2023-07-15 09:26:14 -0300

    feat: incluído o environment de produção

--- GeradorDePDF.WEB/src/app/environments/environment.ts
+++ GeradorDePDF.WEB/src/app/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  // geradorPdfApi: "https://localhost:7122/api",
-  geradorPdfApi: "https://geradordepdf.onrender.com/api"
+  geradorPdfApi: "https://localhost:7122/api",
 }
