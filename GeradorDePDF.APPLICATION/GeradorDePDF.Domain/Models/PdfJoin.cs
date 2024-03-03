911c261771228b2094463c8b1c55de389ed6fe1c
Athos Schrapett <athosschrapett@outlook.com>
2024-03-03 10:39:08 -0300

    feat: ajustes para juntar pdfs

---
+++ GeradorDePDF.APPLICATION/GeradorDePDF.Domain/Models/PdfJoin.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GeradorDePDF.Domain.Models;
+public class PdfJoin(IFormFile pdf, string paginas)
+{
+    public IFormFile? Pdf { get; private set; } = pdf;
+    public int[] Paginas { get; set; } = paginas.Split(',').Select(x => int.Parse(x)).ToArray();
+}
